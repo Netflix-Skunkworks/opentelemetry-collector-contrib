814e247df4636620c15b57d7ea8d9baae8555744
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2021-01-07 14:24:58 -0800

    Add support for percentile summary metrics to the spectatord exporter (#102)

--- exporter/spectatord/adapter.go
+++ exporter/spectatord/adapter.go
@@ -1,22 +1,21 @@
 package spectatord
 
 import (
+	"encoding/json"
 	"fmt"
-	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
-	"github.com/labstack/gommon/log"
 	"net"
-	"go.uber.org/zap"
 	"strconv"
 	"strings"
-)
 
-const (
-	defaultSpecatordProtocol    = "1"
+	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
+	"github.com/hashicorp/go-multierror"
+	"github.com/labstack/gommon/log"
+	"go.uber.org/zap"
 )
 
 var specTypeMapping = map[metricspb.MetricDescriptor_Type]string{
 	metricspb.MetricDescriptor_CUMULATIVE_DOUBLE: "C",
-	metricspb.MetricDescriptor_GAUGE_DOUBLE: "g",
+	metricspb.MetricDescriptor_GAUGE_DOUBLE:      "g",
 }
 
 type Adapter struct {
@@ -45,6 +44,10 @@ func (s *Adapter) UpdateTimeSeries(descriptor *metricspb.MetricDescriptor, serie
 		return s.updateMonotonicCounter(descriptor, series)
 	case metricspb.MetricDescriptor_GAUGE_DOUBLE:
 		return s.updateGauge(descriptor, series)
+	case metricspb.MetricDescriptor_SUMMARY:
+		return s.updateSummary(descriptor, series)
+	case metricspb.MetricDescriptor_CUMULATIVE_DISTRIBUTION:
+		return s.updateCumulativeDistribution(descriptor, series)
 	default:
 		log.Debugf("dropping metric of unexpected type %s:%s", descriptor.GetName(), descriptor.Type)
 		return nil
@@ -52,40 +55,105 @@ func (s *Adapter) UpdateTimeSeries(descriptor *metricspb.MetricDescriptor, serie
 }
 
 func (s *Adapter) updateMonotonicCounter(descriptor *metricspb.MetricDescriptor, series *metricspb.TimeSeries) error {
-	metricName := descriptor.GetName()
-
-	if len(series.Points) != 1 {
-		return fmt.Errorf("skipping update, unexpected number of points in counter metric: %d", len(series.Points))
-	}
-
-	tags := s.getTags(descriptor.GetLabelKeys(), series.GetLabelValues())
-	s.log.Debug(fmt.Sprintf("tags: %+v", tags))
-
-	newCount := series.Points[0].GetDoubleValue()
-	spectatordMsg, err := formatSpectatordMessage(
-		defaultSpecatordProtocol,
-		specTypeMapping[metricspb.MetricDescriptor_CUMULATIVE_DOUBLE],
-		metricName,
-		tags,
-		strconv.FormatFloat(newCount, 'f', 2, 64))
-
-	if err != nil {
-		return fmt.Errorf("failed to format spectatord message: %v", err)
-	} else {
-		s.log.Debug(fmt.Sprintf("writing to spectatord: %s", spectatordMsg))
-		if _, err := s.conn.Write([]byte(spectatordMsg)); err != nil {
-			return fmt.Errorf("failed to write spectatord message to socket: %v", err)
-		}
-	}
-
-	return nil
+	return s.updateSingleValue(metricspb.MetricDescriptor_CUMULATIVE_DOUBLE, descriptor, series)
 }
 
 func (s *Adapter) updateGauge(descriptor *metricspb.MetricDescriptor, series *metricspb.TimeSeries) error {
+	return s.updateSingleValue(metricspb.MetricDescriptor_GAUGE_DOUBLE, descriptor, series)
+}
+
+func (s *Adapter) updateSummary(descriptor *metricspb.MetricDescriptor, series *metricspb.TimeSeries) error {
+	// Example of timeseries data for the summary type
+	// {
+	//	"start_timestamp": {
+	//		"seconds": 1609976233,
+	//		"nanos": 410000000
+	//	},
+	//	"label_values": [{
+	//		"value": "prometheus",
+	//		"has_value": true
+	//	}],
+	//	"points": [{
+	//		"timestamp": {
+	//			"seconds": 1609976248,
+	//			"nanos": 410000000
+	//		},
+	//		"Value": {
+	//			"SummaryValue": {
+	//				"count": {},
+	//				"sum": {},
+	//				"snapshot": {
+	//					"percentile_values": [{
+	//						"percentile": 1,
+	//						"value": 0.000011372
+	//					}, {
+	//						"percentile": 5,
+	//						"value": 0.000011372
+	//					}, {
+	//						"percentile": 50,
+	//						"value": 0.000013092
+	//					}, {
+	//						"percentile": 90,
+	//						"value": 0.000045715
+	//					}, {
+	//						"percentile": 99,
+	//						"value": 0.000084335
+	//					}]
+	//				}
+	//			}
+	//		}
+	//	}]
+	//}
+
+	// spectatord doesn't support passing percentiles directly, instead it assumes it computes this itself from a stream
+	// of values.  So we must split each of percentile int a gauge metric.  We split below by tagging the metrics with "percentile"
+
 	metricName := descriptor.GetName()
 
 	if len(series.Points) != 1 {
-		return fmt.Errorf("skipping update, unexpected number of points in gauge metric: %d", len(series.Points))
+		return fmt.Errorf("skipping update, unexpected number of points in metric: %s:%d", metricName, len(series.Points))
+	}
+
+	var result *multierror.Error
+	const percentile = "percentile"
+	tags := s.getTags(descriptor.GetLabelKeys(), series.GetLabelValues())
+
+	for _, pv := range series.Points[0].GetSummaryValue().Snapshot.PercentileValues {
+		tags[percentile] = strconv.FormatFloat(pv.Percentile, 'f', 2, 64)
+
+		spectatordMsg, err := formatSpectatordMessage(
+			specTypeMapping[metricspb.MetricDescriptor_GAUGE_DOUBLE],
+			metricName,
+			tags,
+			strconv.FormatFloat(pv.Value, 'f', 10, 64))
+
+		if err != nil {
+			result = multierror.Append(fmt.Errorf("failed to format spectatord message: %v", err))
+		} else {
+			if err = s.writeSpectatordMsg(spectatordMsg); err != nil {
+				result = multierror.Append(err)
+			}
+		}
+	}
+
+	if result != nil {
+		return result.ErrorOrNil()
+	} else {
+		return nil
+	}
+}
+
+func (s *Adapter) updateCumulativeDistribution(descriptor *metricspb.MetricDescriptor, series *metricspb.TimeSeries) error {
+	dist, _ := json.Marshal(series)
+	log.Debugf("Ignoring CUMULATIVE DISTRIBUTION: %s: %s", descriptor.GetName(), string(dist))
+	return nil
+}
+
+func (s *Adapter) updateSingleValue(descType metricspb.MetricDescriptor_Type, descriptor *metricspb.MetricDescriptor, series *metricspb.TimeSeries) error {
+	metricName := descriptor.GetName()
+
+	if len(series.Points) != 1 {
+		return fmt.Errorf("skipping update, unexpected number of points in metric: %s:%d", metricName, len(series.Points))
 	}
 
 	tags := s.getTags(descriptor.GetLabelKeys(), series.GetLabelValues())
@@ -93,25 +161,28 @@ func (s *Adapter) updateGauge(descriptor *metricspb.MetricDescriptor, series *me
 
 	newCount := series.Points[0].GetDoubleValue()
 	spectatordMsg, err := formatSpectatordMessage(
-		defaultSpecatordProtocol,
-		specTypeMapping[metricspb.MetricDescriptor_GAUGE_DOUBLE],
+		specTypeMapping[descType],
 		metricName,
 		tags,
-		strconv.FormatFloat(newCount, 'f', 2, 64))
+		strconv.FormatFloat(newCount, 'f', 10, 64))
 
 	if err != nil {
 		return fmt.Errorf("failed to format spectatord message: %v", err)
 	} else {
-		s.log.Debug(fmt.Sprintf("writing to spectatord: %s", spectatordMsg))
-		if _, err := s.conn.Write([]byte(spectatordMsg)); err != nil {
-			return fmt.Errorf("failed to write spectatord message to socket: %v", err)
-		}
+		return s.writeSpectatordMsg(spectatordMsg)
+	}
+}
+
+func (s *Adapter) writeSpectatordMsg(msg string) error {
+	s.log.Debug(fmt.Sprintf("writing to spectatord: %s", msg))
+	if _, err := s.conn.Write([]byte(msg)); err != nil {
+		return fmt.Errorf("failed to write spectatord message: %v", err)
 	}
 
 	return nil
 }
 
-func formatSpectatordMessage(protocol, metricType, name string, tags map[string]string, value string) (string, error) {
+func formatSpectatordMessage(metricType, name string, tags map[string]string, value string) (string, error) {
 	specTags, err := map2SpectatordTags(tags)
 	if err != nil {
 		return "", err
@@ -119,7 +190,6 @@ func formatSpectatordMessage(protocol, metricType, name string, tags map[string]
 
 	// Since spectatord uses ':' to separate fields. They must be removed from all arguments
 	// protocol:metric-type:name#tags:value
-	protocol = strings.Replace(protocol, ":", "_", -1)
 	metricType = strings.Replace(metricType, ":", "_", -1)
 	name = strings.Replace(name, ":", "_", -1)
 	specTags = strings.Replace(specTags, ":", "_", -1)
